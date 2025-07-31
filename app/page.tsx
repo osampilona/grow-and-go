86657c997074286ab78bf06929fc1e79af416b7b
osampilona <brzicamartina@gmail.com>
2025-07-31 12:23:37 +0200

    Tersting

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { Avatar } from "@heroui/react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-row items-top justify-between lg:justify-start min-h-screen w-full gap-2">
+      <h1 className="text-2xl font-bold">Hello, Raphael</h1>
       <Avatar
-        src="https://via.placeholder.com/150"
+        src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
         alt="User Avatar"
-        size="lg"
+        size="md"
       />
     </div>
   );
