4c1145e351037afaf199818de559f8765594478c
osampilona <brzicamartina@gmail.com>
2025-07-31 12:07:53 +0200

    Setup of the project

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Avatar } from "@heroui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Avatar
+        src="https://via.placeholder.com/150"
+        alt="User Avatar"
+        size="lg"
+      />
+    </div>
+  );
+}
