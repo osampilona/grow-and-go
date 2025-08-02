dd0d72b7bf768c0616f84f96ab31e5aa152ebeee
osampilona <brzicamartina@gmail.com>
2025-08-02 13:59:54 +0200

    Smaller changes for navbar breakpoints

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,14 +16,14 @@ export const Navbar = () => {
     <HeroUINavbar maxWidth="2xl" position="sticky" className="bg-fffafa dark:bg-[#282828] backdrop-blur-md">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
+          <NextLink className="flex justify-start items-center" href="/">
             <img src="/logo.png" alt="Grow&Go Logo" className="h-16 w-16 object-contain" />
-            <p className="font-bold text-inherit">Grow&Go</p>
+            <p className="font-bold text-inherit hidden lg:flex">Grow&Go</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full justify-center">
+      <NavbarContent className="hidden lg:flex basis-1/5 sm:basis-full justify-center">
         <NavbarItem>
           <CategoriesList />
         </NavbarItem>
