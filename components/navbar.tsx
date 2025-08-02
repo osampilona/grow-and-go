0e37cab8d5b2b9449de0c530c0b83afc7074462e
osampilona <brzicamartina@gmail.com>
2025-08-02 17:12:38 +0200

    Adding padding top

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ import CategoriesList from "./CategoriesList";
 
 export const Navbar = () => {
   return (
-    <HeroUINavbar maxWidth="2xl" position="sticky" className="bg-fffafa dark:bg-[#282828] backdrop-blur-md">
+    <HeroUINavbar maxWidth="2xl" position="sticky" className="bg-fffafa dark:bg-[#282828] backdrop-blur-md pt-2">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center" href="/">
